671967a3a2bed96e285b4c06d0b91f991ad5ec05
Monk <elmonk.drb@yandex.ru>
2021-04-13 19:56:49 +0300

    Second first commit

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        char[] arr = text.toCharArray();
+        String result ="";
 
-
-    public class Solution {
-
-        public void solution(int number) {
-            //TODO: Code stuff here
-            int a = 0;
-            if (number > 0 && (number % 3 == 0 || number % 5 == 0)) {
-                a = a + number;
-                System.out.println(a);
-            }
-
+        for (int i = arr.length - 1; i >= 0; i--) {
+            result = result + arr[i];
         }
+        System.out.println(result);;
     }
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        String text = scanner.nextLine();
-//        char[] arr = text.toCharArray();
-//        String result ="";
-//
-//        for (int i = arr.length - 1; i >= 0; i--) {
-//            result = result + arr[i];
-//        }
-//        System.out.println(result);;
-//    }
 }
