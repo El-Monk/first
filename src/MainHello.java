1b685444730ecb96899bb81392fc469616cfb2d7
Monk <elmonk.drb@yandex.ru>
2021-04-20 00:03:51 +0300

    first programma

--- src/MainHello.java
+++ src/MainHello.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class MainHello {
     public static void main(String[] args) {
-        System.out.println("branch  ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+
     }
 }
