9a153d63babdcf568790796bbf6a2f8286a21d7d
Monk <elmonk.drb@yandex.ru>
2021-04-20 00:07:04 +0300

    main

--- src/MainHello.java
+++ src/MainHello.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class MainHello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("введите число");
         int number = scanner.nextInt();
 
 
