6e3ce4ce2ebc4eb17ac3b3be24e29d146da6b922
Monk <elmonk.drb@yandex.ru>
2021-04-20 00:28:15 +0300

    Merge branch 'progaramma2'
    # Conflicts:
    #	src/MainHello.java

--- src/MainHello.java
+++ src/MainHello.java
@@ -6,10 +6,10 @@ public class MainHello {
         System.out.println("введите число");
         int number = scanner.nextInt();
 
-        if (number%2 == 0 && number > 0)
+        if (number%2 == 0 && number >= 0)
             System.out.println("вы ввели четное число");
-        else if (number == 0)
-            System.out.println("вы ввели 0");
+        else if (number > 0)
+            System.out.println("число нечетное");
         else
             System.out.println("вы ввели отрицательное число");
     }
